17659a6f741af39953788aec7ea1a969d9e794d5
Ryan Moore <rmoore8849@gmail.com>
2019-07-12 20:12:39 -0500

    starting on Titanic challenge

---
+++ Titanic/predict.py
@@ -0,0 +1,18 @@
+import css
+import model
+
+# make a prediction given the data
+predictions = []
+with open('data.csv') as data_file:
+    data = data_file.read()
+    for row in data:
+        predictions.append(model.make_prediction(row))
+
+# write the data to a file to submit
+fieldnames ['PassengerId', 'Survived']
+with open('predictions.csv', 'w+') as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    for prediction in predictions:
+        writer.writerow(prediction)
+
