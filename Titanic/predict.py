072d0744e3c2ef8f8aeba30e706a3a4da53bcd25
Ryan Moore <rmoore8849@gmail.com>
2019-07-27 02:15:52 -0500

    hyperplane calculations for toy svm

--- Titanic/predict.py
+++ Titanic/predict.py
@@ -9,7 +9,7 @@ with open('./train.csv') as data_file:
         predictions.append(model.make_prediction(row))
 
 # write the data to a file to submit
-fieldnames ['PassengerId', 'Survived']
+fieldnames = ['PassengerId', 'Survived']
 with open('predictions.csv', 'w+') as csv_file:
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
--- Titanic/svm_test.py
+++ Titanic/svm_test.py
@@ -2,7 +2,7 @@
 
 steps:
     1. function that adds labelled data to a plot
-    2. function that tests linear seperability and then decides to keep/remove last dot added
+    2. function that tests linear separability and then decides to keep/remove last dot added
     3. show plot
     4. calculate a hyperplane
     5. calculate the margin
@@ -17,19 +17,72 @@ import matplotlib.cbook as cbook
 get_int = lambda: randint(0, 100)
 
 
-def euclidean_distance(pt_one, pt_two):
-    squared_difference = pow(pt_one[0] - pt_two[0], 2) + pow(pt_one[1] - pt_two[1], 2)
+def get_euclidean_distance(point_one, point_two):
+    """Calculates Euclidean Distance for 2 dimensions, which is the
+    Square root of the sum of the squares of the differences of 2 points
+
+    Arguments:
+      - point_one (2-tuple): first point to compare
+      - point_two (2-tuple): second point to compare
+
+    Returns:
+      - (int): the euclidean distance of two points
+    """
+    squared_difference = pow(point_one[0] - point_two[0], 2) + pow(point_one[1] - point_two[1], 2)
     return math.sqrt(squared_difference)
 
-def get_hyperplane(pt_one, pt_two):
-    """ y = mx + b
-    """
-    z_vector = {
-        'x': pt_one[0] - pt_two[0],
-        'y': pt_one[1] - pt_two[1],
-    }
-    hyperplane = ()
 
+def get_hyperplane(point_one, point_two):
+    """Generate a hyperplane between two points.
+    This is achieved by finding a line that goes through the
+    median segment between both points, that is also orthogonal 
+    to the median segment.
+
+    A hyperplane in 2-dimensional space is of the form:
+    x1 =  m*x2 + b
+    
+    Arguments:
+      - point_one (2-tuple): first point used to generate hyperplane
+      - point_two (2-tuple): second point to generate hyperplane
+
+    Returns:
+      (hyperplane): a fucking hyperplane
+    """
+    # median segment
+    ms = {
+        'x1': point_one[0] - point_two[0],
+        'x2': point_one[1] - point_two[1],
+    }
+
+    # mid-point
+    mp = {
+        'x1': (point_one[0] + point_two[0])/2,
+        'x2': (point_one[1] + point_two[1])/2,
+    }
+
+    # perpendicular vector
+    p = {
+        'x1': -mp['x2'],
+        'x2':  mp['x1'],
+    }
+
+    p_sqrd = pow(p['x1'], 2) + pow(p['x2'], 2)
+    mp_sqrd = pow(mp['x2'], 2) + pow(mp['x2'], 2)
+
+    p_coeff = (p['x1'] * mp['x1'] + p['x2'] * mp['x2'])/p_sqrd
+    mp_coeff = (ms['x1'] * mp['x1'] + ms['x1'] * mp['x2'])/p_sqrd
+    
+    
+    p = {
+        'x1': coeff * p['x1'],
+        'x2': coeff * p['x2'],
+    }
+
+    # y = mx + b
+
+    return p
+    
+    
 
 
 def is_separable(data):
@@ -42,7 +95,6 @@ def is_separable(data):
     for pt_one in set_1:
         for pt_two in set_2_pt:
             hyper_plane = get_hyperplane(pt_one, pt_two)
-
 
 
 def generate_separable_plot(num_pts):
@@ -70,9 +122,9 @@ def make_plt(data):
     fig, ax = plt.subplots()
     ax.scatter(x1s, x2s, c=c)
 
-    ax.set_xlabel('Shrim (lbs)', fontsize=15)
-    ax.set_ylabel('Plumbis Length(m)', fontsize=15)
-    ax.set_title('FUCKING DOTS')
+    ax.set_xlabel('Unit 1 (K)', fontsize=15)
+    ax.set_ylabel('Unit 2 (K)', fontsize=15)
+    ax.set_title('Graph of things')
 
     ax.grid(True)
     fig.tight_layout()
